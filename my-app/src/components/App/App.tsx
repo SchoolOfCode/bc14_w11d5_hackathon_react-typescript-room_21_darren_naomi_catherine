54f8dd1a24ab8b6533f842e4bf96281201edac64
Darren Broomhall <d.p.broomhall@gmail.com>
2023-05-26 15:19:48 +0100

    App functional

--- my-app/src/components/App/App.tsx
+++ my-app/src/components/App/App.tsx
@@ -20,24 +20,28 @@ function App() {
   //helper functions go here
   async function fetchBothAPIs(search: string) {
     // first API call
-    const url1 = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=1&appid=03eeaa1ba18c8cd073330aef774df3e8`;
-    const response1 = await fetch(url1);
-    const data1 = await response1.json();
-    // might need [0]
-    const name = data1.name;
-    const country = data1.country;
-    const lat = data1.lat;
-    const lon = data1.lon;
+    // const url1 = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=1&appid=4bba526eeac67e7e53995debc7662040`;
+    // const response1 = await fetch(url1);
+    // const data1 = await response1.json();
+    // // might need [0]
+    // const name = data1[0].name;
+    // const country = data1[0].country;
+    // const lat = data1[0].lat;
+    // const lon = data1[0].lon;
+    // console.log(data1)
 
     // second API call
-    const url2 = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=03eeaa1ba18c8cd073330aef774df3e8`;
+    const url2 = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&APPID=03eeaa1ba18c8cd073330aef774df3e8`;
 
     const response2 = await fetch(url2);
     const data2 = await response2.json();
-    const temp = data2.current.temp;
-    const feels_like = data2.current.feels_like;
+    const temp = data2.main.temp;
+    const feels_like = data2.main.feels_like;
     const main = data2.weather.main;
     const icon = data2.weather.icon;
+    const name = data2.name;
+    const country = data2.sys.country;
+    console.log(data2)
     const weatherData = {
       name,
       country,
@@ -53,6 +57,7 @@ function App() {
   return (
     <div className="App">
       <main>
+      <h1>Weather App</h1>
         <Search fetchBothAPIs={fetchBothAPIs} />
         { weather && <Display weather={weather} />}
         <section>
--- my-app/src/components/Display/Display.tsx
+++ my-app/src/components/Display/Display.tsx
@@ -15,13 +15,13 @@ export default function Display(props: WeatherDataState) {
 
   return (
     <div>
-      <h1>Weather App</h1>
+    
       <h2>
         {weather.name}, {weather.country}
       </h2>
       <img src={weather.icon} alt="weather icon" />
-      <h2>{weather.temp}</h2>
-      <h4>{weather.feels_like}</h4>
+      <h2>Temperature {weather.temp}°C</h2>
+      <h4>Feels Like {weather.feels_like}°C</h4>
       <h4>{weather && weather.main}</h4>
       {/* conditional rendering above is optional - we have rendered in app*/}
       <h3>TIME</h3>
